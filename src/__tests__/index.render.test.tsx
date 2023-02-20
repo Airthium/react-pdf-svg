981d2ee2f26beb2fec2b94aecd2ed41d3da06f7b
sgarnotel <simon.garnotel@gmail.com>
2023-02-20 09:32:28 +0100

    v0.9.13

--- src/__tests__/index.render.test.tsx
+++ src/__tests__/index.render.test.tsx
@@ -10,5 +10,5 @@ describe('index - render', () => {
   test('test', async () => {
     await import('..')
     await new Promise((resolve) => setTimeout(resolve, 2_000))
-  }, 7_000)
+  }, 10_000)
 })
